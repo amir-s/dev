cd894e2aca137ef456684dac0d1ead3fc43aff41
Amir Saboury <amir.saboury@gmail.com>
2022-04-29 17:23:37 -0400

    set the DEV_CLI_BIN_PATH env var only for the duration of the execution of the binary

--- shell/install.sh
+++ shell/install.sh
@@ -1,6 +1,4 @@
 <$SHELL_FN_NAME$>() {
-  export DEV_CLI_BIN_PATH="<$SHELL_BIN_PATH$>"
-
   local tempfile exitcode cmd
 
   tempfile="$(mktemp -u)"
@@ -8,7 +6,7 @@
   exec 8<"${tempfile}"
   rm -f "${tempfile}"
 
-  <$SHELL_BIN_PATH$> "$@"
+  DEV_CLI_BIN_PATH="<$SHELL_BIN_PATH$>" <$SHELL_BIN_PATH$> "$@"
   exitcode=$?
 
   while read -r cmd; do
