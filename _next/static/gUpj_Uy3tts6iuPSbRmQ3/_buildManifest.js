60994c25cf2d65d76c024cdda5f025d40988cdbb
Amir Saboury <amir.saboury@gmail.com>
2022-04-30 01:45:43 -0400

    Updates

---
+++ _next/static/gUpj_Uy3tts6iuPSbRmQ3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-6941408eef9b7df5.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
