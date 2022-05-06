39125746ccb1d537a9e84ec61131f146ac0a3637
Amir Saboury <amir.saboury@gmail.com>
2022-05-05 21:44:46 -0400

    check for empty temp file in fish

--- shell/install.fish
+++ shell/install.fish
@@ -1,18 +1,22 @@
 function <$SHELL_FN_NAME$>
     set -l tempfile (mktemp -u)
   
-    env DEV_CLI_BIN_PATH=<$SHELL_BIN_PATH$>, DEV_CLI_CMD_EXEC_FILE=$tempfile <$SHELL_BIN_PATH$> $argv
+    env DEV_CLI_BIN_PATH="<$SHELL_BIN_PATH$>" DEV_CLI_CMD_EXEC_FILE="$tempfile" <$SHELL_BIN_PATH$> $argv
     set -l exitcode $status
   
-    while read cmd
-        echo $cmd
-        switch $cmd
-            case "cd:*"
-               cd (string replace -r "cd:" "" -- $cmd)
-            case "*"
-               # echo do nothing
-        end
-    end <$tempfile
+    if test -f $tempfile
+        while read cmd
+            switch $cmd
+                case "cd:*"
+                cd (string replace -r "cd:" "" -- $cmd)
+                case "*"
+                # echo do nothing
+            end
+        end <$tempfile
+    end
+    
+    # add an empty line to the end of the output
+    echo ""
 
     rm -f $tempfile
     return $exitcode
