b164d2f9a4f9946e0ecc6eb56311e8afb8636db4
Amir Saboury <amir.saboury@gmail.com>
2022-05-08 00:50:33 -0400

    added tests for dev clone

--- contextual/index.mjs
+++ contextual/index.mjs
@@ -1,13 +1,19 @@
-import { $ } from "zx";
+import { $, nothrow } from "zx";
 
 $.verbose = true;
 
 export const run = async ({ args, config }) => {
+  process.env.FORCE_COLOR = "1";
+
   const useYarn = config("contextual.node.yarn", false);
 
-  if (useYarn) {
-    await $`yarn run ${args}`;
-  } else {
-    await $`npm run ${args}`;
+  try {
+    if (useYarn) {
+      await $`yarn run ${args}`;
+    } else {
+      await $`npm run ${args}`;
+    }
+  } catch (e) {
+    process.exit(e.exitCode);
   }
 };
