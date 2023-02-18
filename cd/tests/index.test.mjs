81c5112876fcbdab72c4621d57d437b4e7902ddd
Amir Saboury <amir.saboury@gmail.com>
2023-02-18 02:55:42 -0500

    add failing tests to `dev cd` command

--- cd/tests/index.test.mjs
+++ cd/tests/index.test.mjs
@@ -35,4 +35,38 @@ describe("findMatch", () => {
     );
     assert.deepEqual({ user: "amir-s", repo: "123abc1230" }, repo);
   });
+
+  test("prefers repo and query to have continues matches", () => {
+    const { repo } = findMatch(
+      [
+        {
+          user: "star",
+          repo: "aba",
+        },
+        {
+          user: "amir-s",
+          repo: "saba",
+        },
+      ],
+      "saba"
+    );
+    assert.deepEqual({ user: "amir-s", repo: "saba" }, repo);
+  });
+
+  test("prefers repo and query to match both repo and user somehow", () => {
+    const { repo } = findMatch(
+      [
+        {
+          user: "str-blah",
+          repo: "blah-blah",
+        },
+        {
+          user: "str-blah",
+          repo: "blah-str-blah",
+        },
+      ],
+      "str"
+    );
+    assert.deepEqual({ user: "str-blah", repo: "blah-str-blah" }, repo);
+  });
 });
