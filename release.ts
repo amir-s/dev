65ab67f68bba69743e48760eee5b7a8c4d275975
Amir Saboury <amir.saboury@gmail.com>
2024-11-10 16:50:14 -0500

    modified release script

--- release.ts
+++ release.ts
@@ -1,17 +1,21 @@
+import "colors";
+import inquirer from "inquirer";
 import { getCurrentVersion } from "./utils/version.ts";
+import semver from "semver";
 
-const denoConfig = JSON.parse(await Deno.readTextFile("deno.json"));
-const denoJsonVersion = `v${denoConfig.version}`;
+const VERSION_TS_FILE = "./utils/version.ts";
 
 const decoder = new TextDecoder();
 
-const version = `v${getCurrentVersion()}`;
+const denoConfig = JSON.parse(await Deno.readTextFile("deno.json"));
+const denoJsonVersion = `v${denoConfig.version}`;
+const currentVersion = `v${getCurrentVersion()}`;
 
-if (version !== denoJsonVersion) {
+if (currentVersion !== denoJsonVersion) {
   console.error(
-    "Version mismatch between current version and deno.json version",
+    "Version mismatch between current version and deno.json version.",
     {
-      version,
+      currentVersion,
       denoJsonVersion,
     }
   );
@@ -19,8 +23,46 @@ if (version !== denoJsonVersion) {
   Deno.exit(1);
 }
 
+const { releaseType } = await inquirer.prompt([
+  {
+    type: "list",
+    name: "releaseType",
+    message: `What type of release is this? (Current version is ${currentVersion.green})`,
+    choices: ["patch", "minor", "major"].map((choice) => ({
+      name: `${choice} (${semver.inc(denoConfig.version, choice)})`,
+      value: choice,
+    })),
+  },
+]);
+
+if (!releaseType) {
+  console.error("Invalid release type");
+  Deno.exit(1);
+}
+
+const newVersion = semver.inc(denoConfig.version, releaseType);
+
+if (!newVersion) {
+  console.error("Invalid new version");
+  Deno.exit(1);
+}
+
+console.log(` Releasing version v${newVersion.green}`);
+
+// change version in version.ts
+const versionFile = await Deno.readTextFile(VERSION_TS_FILE);
+const newVersionFile = versionFile.replace(
+  `/**/ "${denoConfig.version}"; /**/`,
+  `/**/ "${newVersion}"; /**/`
+);
+await Deno.writeTextFile(VERSION_TS_FILE, newVersionFile);
+
+// change version in deno.json
+denoConfig.version = newVersion;
+await Deno.writeTextFile("deno.json", JSON.stringify(denoConfig, null, 2));
+
 const commitCommand = new Deno.Command("git", {
-  args: ["commit", "--allow-empty", "-am", `Release ${version}`],
+  args: ["commit", "--allow-empty", "-am", `Release v${newVersion}`],
   stdout: "piped",
   stderr: "piped",
 });
@@ -40,7 +82,7 @@ if (commitCode !== 0 || commitError.length > 0) {
 console.log(decoder.decode(commitOutput));
 
 const tagCommand = new Deno.Command("git", {
-  args: ["tag", "-a", version, "-m", `Version ${version} release`],
+  args: ["tag", "-a", `v${newVersion}`, "-m", `Version v${newVersion} release`],
   stdout: "piped",
   stderr: "piped",
 });
@@ -78,4 +120,4 @@ if (pushCode !== 0 || pushError.length > 0) {
 
 console.log(decoder.decode(pushOutput));
 
-console.log(`Successfully tagged and pushed ${version}`);
+console.log(`Successfully tagged and pushed ${newVersion.green}`);
